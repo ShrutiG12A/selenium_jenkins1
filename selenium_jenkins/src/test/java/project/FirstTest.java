34cd15b47a3d5221af006c989a556f7760b3c850
aadit <aadit@Shruti-pc1>
2024-08-16 12:03:21 +0530

    poll scm

--- selenium_jenkins/src/test/java/project/FirstTest.java
+++ selenium_jenkins/src/test/java/project/FirstTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class FirstTest 
@@ -15,6 +16,7 @@ WebDriver driver=new ChromeDriver();
 driver.manage().window().maximize();
 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 driver.get("https://www.google.com/");
+Reporter.log("first class is executing",true );
 }
 }
 
---
+++ selenium_jenkins/src/test/java/project/NextClassTest.java
@@ -0,0 +1,15 @@
+package project;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.google.common.annotations.VisibleForTesting;
+
+public class NextClassTest
+{
+@Test
+public void sample()
+{
+	Reporter.log("sample test is executing ",true );
+}
+}
